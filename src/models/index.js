f460a39e262c0ab6678874269e2a9b61abeef3a8
Joao Lucas <jlcarruda3@gmail.com>
2020-05-14 13:18:42 -0300

    Implemented the table model

--- src/models/index.js
+++ src/models/index.js
@@ -4,8 +4,10 @@ const modelFactory = new ModelFactory()
 
 const user = require('./blueprints/user')
 const character = require('./blueprints/character')
+const table = require('./blueprints/table')
 
 module.exports = {
   User: modelFactory.create(user),
-  Character: modelFactory.create(character)
+  Character: modelFactory.create(character),
+  Table: modelFactory.create(table)
 }
